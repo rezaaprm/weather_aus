13cd17da3683cf4c51e140c3472864c87cd12834
rezaaprm <reza310105@gmail.com>
2025-08-16 09:34:11 +0700

    -

---
+++ Streamlit/streamlit.py
@@ -0,0 +1,42 @@
+# Streamlit Library
+import streamlit as st
+
+# Main Library
+import numpy as np 
+import pandas as pd
+
+# Visualization Library
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+# Page Config
+st.set_page_config(layout='wide')
+
+st.title("Dashboard of Weather Australia using Streamlit")
+
+df = pd.read_csv("../Dataset/cleaned_weatherAUS.csv")
+
+# Columns
+col1, col2 = st.columns([0.5, 0.5], gap="medium")
+
+with col1:
+    st.info("Rain Tomorrow Countplot")
+    fig, ax = plt.subplots(figsize=(8, 4))
+    sns.countplot(df, x="RainTomorrow", hue="RainTomorrow")
+    ax.set_xlabel('')
+    ax.set_ylabel('')
+    ax.set_title('')
+    ax.grid(True)
+    st.pyplot(fig)
+
+with col2:
+    st.info("Rain Today Countplot")
+
+    fig, ax = plt.subplots(figsize=(8, 4))
+    sns.countplot(df, x="RainToday", hue="RainToday")
+    ax.set_xlabel('')
+    ax.set_ylabel('')
+    ax.set_title('')
+    ax.grid(True)
+    st.pyplot(fig)
